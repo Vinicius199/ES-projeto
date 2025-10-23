f3dcaa2a7de8eae591e36cfb15c07fa95602c136
Vinicius Ribeiro <viniciussa.alunos.utfpr.edu.br>
2025-10-23 13:38:17 -0300

    feito alguns ajustes e incrementando alguns templetes

---
+++ templetes/static/js/main.js
@@ -0,0 +1,12 @@
+// Referências aos botões
+const btnLogin = document.getElementById('btnLogin');
+const btnCadastro = document.getElementById('btnCadastro');
+
+// Eventos de clique (simples)
+btnLogin.addEventListener('click', () => {
+  //alert('Redirecionar para tela de login');
+});
+
+btnCadastro.addEventListener('click', () => {
+  //alert('Redirecionar para tela de cadastro');
+});
